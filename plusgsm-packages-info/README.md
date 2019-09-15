67c1d7b093a0598280bb6ba3aa3a6ee184678fb6
Mariusz Strzelecki <szczeles@gmail.com>
2019-09-15 13:07:36 +0200

    plusgsm-packages-info

---
+++ plusgsm-packages-info/README.md
@@ -0,0 +1,8 @@
+# PlusGSM packages info
+
+Script for monitoring usage of "limitless" LTE package
+
+## Crontab
+
+    07 * * * * /home/osmc/toolbelt/plusgsm-packages-info/app.py [user] [pass] >> /home/osmc/pakiety.log
+
---
+++ plusgsm-packages-info/app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import requests, re, datetime, argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("phone_number")
+parser.add_argument("password")
+args = parser.parse_args()
+
+print(datetime.datetime.now())
+with requests.Session() as s:
+    s.post('https://ssl.plusgsm.pl/ebok-web/basic/loginStep2.action', 
+        data={'msisdn': args.phone_number, 'password': args.password, 'brandId': ''}
+    )
+    html = s.get('https://ssl.plusgsm.pl/ebok-web/spectrum/packages/show.action').content.decode('utf-8')
+    packages = re.findall("<span class=\"text_12_bold\">\s+(.*)\s+</span>\s+</td>\s+" + 
+       "<td>\s+<span class=\"text_12\">\s+<ul>\s+<li>\s+Dla telefonu <[^<]+</li>\s+</ul>\s+</span>\s+</td>\s+" + 
+       "<td align=\"center\">\s+<span class=\"text_12\">\s+([0-9,]+) GB\s+", html)
+    for package in packages:
+        print("%s: %.2f GB" % (package[0].strip(), float(package[1].replace(',', '.'))))
+
+print()
