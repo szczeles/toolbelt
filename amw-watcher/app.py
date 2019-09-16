f180f7f7f71577436bcf834a9ed79760f318b31f
Mariusz Strzelecki <szczeles@gmail.com>
2019-09-16 22:26:15 +0200

    AMW shoes sizes availability watcher

---
+++ amw-watcher/app.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+import requests
+import re
+import json
+import argparse
+from spamer import Spamer
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--gmail-user')
+parser.add_argument('--gmail-password')
+parser.add_argument('--rcpt')
+parser.add_argument('--state-file')
+args = parser.parse_args()
+
+spamer = Spamer(args.gmail_user, args.gmail_password, args.rcpt)
+
+urls = [
+    ('https://sklep.amw.com.pl/pl/p/Trzewiki-desantowca/117', 'Desanty'),
+    ('https://sklep.amw.com.pl/pl/p/Trzewiki-pilota-wz.-922MON-/216', 'Trzewiki pilota'),
+    ('https://sklep.amw.com.pl/pl/p/Buty-specjalne-928MON-/119', 'Buty specjalne')
+]
+
+email = ''
+
+for url, title in urls:
+    response = requests.get(url).text
+    match = re.search('Shop.values.OptionsConfiguration = "([^"]*)"', response)
+    available_sizes_keys = json.loads(match.group(1))
+    available_sizes = []
+    for size in re.finditer('<option value="([^"]+)">([^<]+)</option>', response):
+        if int(size.group(1)) in available_sizes_keys:
+            available_sizes.append(size.group(2))
+    email += '<h1>{}</h1>'.format(title)
+    email += '<p>{}</p>'.format(available_sizes)
+
+try:
+    with open(args.state_file) as f:
+        previous_state = f.read()
+except:
+    previous_state = None
+
+if previous_state != email:
+    spamer.send(email)
+
+with open(args.state_file, 'w') as f:
+    f.write(email)
---
+++ amw-watcher/spamer.py
@@ -0,0 +1,27 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+class Spamer:
+
+    def __init__(self, gmail_user, gmail_pass, rcpt):
+        self.gmail_user = gmail_user
+        self.gmail_pass = gmail_pass
+        self.recipients = [rcpt]
+
+    def send(self,  content):
+        msg = MIMEMultipart("alternative")
+        msg['Subject'] = 'Nowe buty od AMW!'
+        msg['From'] = 'AMW watcher'
+        msg['To'] = self.recipients[0]
+        part1 = MIMEText(content, 'html', 'utf-8')
+        msg.attach(part1)
+
+        smtpserver = smtplib.SMTP('smtp.gmail.com',587)
+        smtpserver.ehlo()
+        smtpserver.starttls()
+        smtpserver.login(self.gmail_user, self.gmail_pass)
+        smtpserver.sendmail(self.recipients[0], self.recipients, msg.as_string())
+        smtpserver.close()
+
