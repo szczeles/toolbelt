fddb01af7d1bb7c7be8fa6f0514b57135100432e
Mariusz Strzelecki <szczeles@gmail.com>
2019-11-08 19:34:50 +0100

    Librus messages notifier

---
+++ librus-messages-notifier/README.md
@@ -0,0 +1,9 @@
+# Messages watcher for Librus
+
+Setup via crontab with:
+
+    python3 app.py \
+        --librus-user 12345 --librus-password PASS \
+        --gmail-user xxx --gmail-pass xxxx --rcpt xxxxx,yyyy \
+        --state-file /tmp/librus
+
---
+++ librus-messages-notifier/app.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+
+import argparse
+from spamer import Spamer
+from librus import LibrusSession
+from difflib import Differ
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--librus-user')
+parser.add_argument('--librus-password')
+parser.add_argument('--gmail-user')
+parser.add_argument('--gmail-password')
+parser.add_argument('--rcpt')
+parser.add_argument('--state-file')
+args = parser.parse_args()
+
+
+session = LibrusSession()
+session.login(args.librus_user, args.librus_password)
+
+current_state = []
+for message in session.list_messages(get_content=True):
+    current_state.extend([
+        f'Nadawca: {message.sender}',
+        f'Temat: {message.subject}',
+        f'Wysłano: {message.sent_at}',
+        f'Wiadomość: ' + message.content.replace("\n", "<br/>"),
+        '<hr/>'
+    ])
+
+try:
+    with open(args.state_file) as f:
+        previous_state = [line.strip() for line in f.readlines()]
+except:
+    previous_state = []
+
+if previous_state != current_state:
+    diff = list(Differ().compare(previous_state, current_state))
+    email = '<br />\n'.join(diff)
+    print(email)
+    spamer = Spamer(args.gmail_user, args.gmail_password, args.rcpt)
+    spamer.send(email)
+else:
+    print("No change")
+
+with open(args.state_file, 'w') as f:
+    for line in current_state:
+        f.write(f'{line}\n')
